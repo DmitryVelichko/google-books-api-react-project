068b626228564287f3fbef8ad192a702b5369415
Dmitry Velichko <soleil-2005@mail.ru>
2021-08-14 15:16:11 +0300

    New Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./App.css";
+import { InputGroup, Input, InputGroupAddon } from 'reactstrap';
 
 function App() {
   const mainHeader = () => {
     return (
       <div className="main-image d-flex justify-content-center align-items-center flex-column">
-        <div className="filter">AAA</div>
+        <div className="filter"></div>
+        <h1 className="googleBooks display-2 text-center text-white mb-3">Гугол книги</h1>
+        <div className="inputContainer">
+        <InputGroup className='mb-3' size='lg'>
+          <Input placeholder='Найти книгу'/>
+        </InputGroup>
+        </div>
       </div>
     );
   }
