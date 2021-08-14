4dcc7fa49399afb3e4f40cf53e707762d74b515f
Dmitry Velichko <soleil-2005@mail.ru>
2021-08-14 14:59:50 +0300

    New Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Google Books</h1>
-    </div>
-  );
+  const mainHeader = () => {
+    return (
+      <div className="main-image d-flex justify-content-center align-items-center flex-column">
+        <div className="filter">AAA</div>
+      </div>
+    );
+  }
+  return <div>{mainHeader()}</div>;
 }
-
 export default App;
