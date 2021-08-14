6dd67bed0839160efd395eed0c23dff7f129c765
Dmitry Velichko <soleil-2005@mail.ru>
2021-08-14 15:42:50 +0300

    New Commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from "react";
 import "./App.css";
-import { InputGroup, Input, InputGroupAddon } from 'reactstrap';
+import { InputGroup, Input, InputGroupAddon, Button } from "reactstrap";
 
 function App() {
   const mainHeader = () => {
     return (
       <div className="main-image d-flex justify-content-center align-items-center flex-column">
         <div className="filter"></div>
-        <h1 className="googleBooks display-2 text-center text-white mb-3">Гугол книги</h1>
+        <h1 className="googleBooks display-2 text-center text-white mb-3">
+          Гугол книги
+        </h1>
         <div className="inputContainer">
-        <InputGroup className='mb-3' size='lg'>
-          <Input placeholder='Найти книгу'/>
-        </InputGroup>
+          <InputGroup className="mb-3" size="lg">
+            <Input placeholder="Найти книгу" />
+            <InputGroupAddon addonType="append">
+              <Button color="secondary">
+                <i className="fas fa-search"></i>
+              </Button>
+            </InputGroupAddon>
+          </InputGroup>
         </div>
       </div>
     );
-  }
+  };
   return <div>{mainHeader()}</div>;
 }
 export default App;
