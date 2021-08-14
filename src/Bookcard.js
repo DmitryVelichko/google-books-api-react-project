84af2a4c407c71b6617b77958437674ac08d87f8
Dmitry Velichko <soleil-2005@mail.ru>
2021-08-14 17:48:13 +0300

    New Commit

--- src/Bookcard.js
+++ src/Bookcard.js
@@ -15,7 +15,33 @@ const Bookcard = ({
   }) => {
     return (
         <div>
-            
+            // States
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
+
+  return (
+    <Card style={{ width: '233px' }} className='m-auto '>
+      <CardImg
+        top
+        style={{ width: '100%', height: '233px' }}
+        src={thumbnail}
+        alt={title}
+      />
+      <CardBody>
+        <CardTitle className='card-title'>{title}</CardTitle>
+        <Button onClick={toggle}>More info</Button>
+      </CardBody>
+      <Modal isOpen={modal} toggle={toggle}>
+        <div className='modal-header d-flex justify-content-center'>
+          <h5 className='modal-title text-center' id='exampleModalLabel'>
+            {title}
+          </h5>
+          <button
+            aria-label='Close'
+            className='close'
+            type='button'
+            onClick={toggle}
+          ></button>
         </div>
     )
 }
