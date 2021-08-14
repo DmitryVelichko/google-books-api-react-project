37cf2ba492298e75adb02dd4e9f342ef4f47f7b6
Dmitry Velichko <soleil-2005@mail.ru>
2021-08-14 17:50:28 +0300

    New Commit

--- src/App.js
+++ src/App.js
@@ -73,6 +73,22 @@ function App() {
         </div>
       </div>
     );
+
+    return (
+      <div className='col-lg-4 mb-3' key={item.id}>
+        <BookCard
+          thumbnail={thumbnail}
+          title={item.volumeInfo.title}
+          pageCount={item.volumeInfo.pageCount}
+          language={item.volumeInfo.language}
+          authors={item.volumeInfo.authors}
+          publisher={item.volumeInfo.publisher}
+          description={item.volumeInfo.description}
+          previewLink={item.volumeInfo.previewLink}
+          infoLink={item.volumeInfo.infoLink}
+        />
+      </div>
+    );
   };
   return <div>{mainHeader()}</div>;
 }
